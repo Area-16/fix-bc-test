0f296d93b127b6c4b44faceae5b9c1f08786b8d1
Douglas Alves <douglas.expalves@gmail.com>
2017-12-18 20:16:11 -0200

    added trainer model

---
+++ src/db/models/trainer.js
@@ -0,0 +1,66 @@
+import sequelize from '../index'
+import Sequelize from 'sequelize'
+
+let Trainers = sequelize.define('trainer', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  country: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: 'br'
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  zip_code: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  street: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  street_number: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  doc_type: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: 'cpf'
+  },
+  doc_num: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+},
+{
+  freezeTableName: true
+})
+
+Trainers.sync({ force: false })
+  .then(() => {})
+  .catch(() => console.log('Trainers model sync error'))
+export default Trainers
